a110718eadb650e2b4adc2051a414dc3f1019cc9
Bilal094 <99066318@mydavinci.nl>
2023-02-24 14:21:34 +0100

    finished assignment

---
+++ js/piramide_aflopend.js
@@ -0,0 +1,18 @@
+var title = document.createElement("h1");
+var arrDisplay = document.createElement("p");
+let numArr = [];
+let num = prompt("Voer een getal in:");
+
+for (let i = 0; i <= num; i++) {
+    numArr.push(i);
+}
+
+arrDisplay.innerHTML = "Array : " + numArr + "<br>";
+document.body.appendChild(arrDisplay);
+
+for (let i = num; i >= 0 ; i--) {
+    for (let x = 0; x <= i; x++) {
+        arrDisplay.innerHTML += numArr[x];
+    }
+    arrDisplay.innerHTML += "<br />";
+}
